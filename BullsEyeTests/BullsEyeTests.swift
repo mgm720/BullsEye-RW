d7c65ef225170ce8bfcb753d81ad4287e29e39f3
Michael Miles <michael.gage.miles@gmail.com>
2019-04-25 19:19:43 -0700

    Initial Commit

---
+++ BullsEyeTests/BullsEyeTests.swift
@@ -0,0 +1,61 @@
+//
+//  BullsEyeTests.swift
+//  BullsEyeTests
+//
+//  Created by Michael Miles on 4/25/19.
+//  Copyright © 2019 Ray Wenderlich. All rights reserved.
+//
+
+import XCTest
+@testable import BullsEye
+
+class BullsEyeTests: XCTestCase {
+    
+    var gameUnderTest: BullsEyeGame!
+
+    override func setUp() {
+        gameUnderTest = BullsEyeGame()
+        gameUnderTest.startNewGame()
+    }
+
+    override func tearDown() {
+        gameUnderTest = nil
+    }
+
+    // XCTAssert to test model
+    //Alternative naming systems are Arrange-Act-Assert and Assemble-Activate-Assert.
+    func testScoreIsComputed() {
+        // 1. given
+        let guess = gameUnderTest.targetValue + 5
+        
+        // 2. when
+        _ = gameUnderTest.check(guess: guess)
+        
+        // 3. then
+        XCTAssertEqual(gameUnderTest.scoreRound, 95, "Score computed from guess is wrong")
+    }
+    
+    func testScoreIsComputedWhenGuessGTTarget() {
+        // 1. given
+        let guess = gameUnderTest.targetValue + 5
+        
+        // 2. when
+        _ = gameUnderTest.check(guess: guess)
+        
+        // 3. then
+        XCTAssertEqual(gameUnderTest.scoreRound, 95, "Score computed from guess is wrong")
+    }
+    
+    func testScoreIsComputedWhenGuessLTTarget() {
+        // 1. given
+        let guess = gameUnderTest.targetValue - 5
+        
+        // 2. when
+        _ = gameUnderTest.check(guess: guess)
+        
+        // 3. then
+        XCTAssertEqual(gameUnderTest.scoreRound, 95, "Score computed from guess is wrong")
+    }
+
+
+}
